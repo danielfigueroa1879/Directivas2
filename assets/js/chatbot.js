5b9dcdcf021e8b7cc2aedeb64047d6fd60da7400
danielfigueroa1879 <90109125+danielfigueroa1879@users.noreply.github.com>
2025-07-14 00:03:46 -0400

    Update chatbot.js

--- assets/js/chatbot.js
+++ assets/js/chatbot.js
@@ -18,137 +18,137 @@ const chatWidgetContainer = document.getElementById('chat-widget-container');
 
 
 // --- Predefined Responses ---
-// Base de datos de respuestas predefinidas
+// Base de datos de respuestas predefinidas, actualizada con el nuevo documento.
 const predefinedResponses = {
-    'figueroa_descanso': {
+    'rule_1': {
         keywords: ['figueroa', 'mi suboficial', 'mi sargento'],
         response: '🧙🏻 ‍♂️ no estoy de servicio, puede llamar al 512651024 o esperar la atención de lunes a viernes de 08:30 a 13:00 horas. Gracias  👍✌️'
     },
-    'descanso_viernes': {
+    'rule_2': {
         keywords: ['*mi suboficial*', '*mi sargento*'],
         response: 'Estoy con unos días de descanso vuelvo el viernes, saludos  🙂   👍🏼'
     },
-    'fuera_de_servicio': {
+    'rule_3': {
         keywords: ['*servicio*'],
         response: 'Hola, esta es una contestadora automática, no estoy de servicio puede llamar al 512651024 o esperar la atención de lunes a viernes de 08:30 a 13:00 horas. Gracias  👍'
     },
-    'saludo_automatico': {
+    'rule_4': {
         keywords: ['*hola*'],
         response: 'Hola esta es una contestadora automática, deja tu mensaje y te escribiré cuando pueda, gracias, sigamos cuidándonos  🙏👍💪'
     },
-    'horario_atencion_general': {
+    'rule_5': {
         keywords: ['*horario*', '*atención*', '*horarios*'],
         response: 'Hola, puede llamar al 512651024 o esperar la atención de lunes a viernes de 09:00 a 13:00 horas. Gracias  👍'
     },
-    'guias_componentes': {
+    'rule_6': {
         keywords: ['guías', 'guía', 'componentes del sistema', 'componentes'],
         response: '*ESCRIBA EL NOMBRE DEL COMPONENTE DEL SISTEMA Y SE DESCARGARA UNA GUIA, PARA QUE PUEDA REALIZAR SU TRAMITE* 👮🏻 ‍♂️ \n  ⬇️ \n*1.-* VIGILANTE PRIVADO\n*2.-* GUARDIA DE SEGURIDAD\n*3.-* JEFE DE SEGURIDAD \n*4.-* ENCARGADO DE SEGURIDAD\n*5.-* SUPERVISOR\n*6.-* ASESOR \n*7.-* CAPACITADOR\n*8.-* TÉCNICO \n*9.-* OPERADOR DE CAJEROS \n*10.-* INSTALADOR TÉC. DE SEGURIDAD\n*11.-* OPERADOR CCTV.\n*12.-* EMPRESAS'
     },
-    'guia_guardia': {
+    'rule_7': {
         keywords: ['guardia de seguridad', 'guardia', 'guardia seguridad'],
         response: '🤖   🧙🏻 ‍♂️ Ok... en este link encontrará la guía de *GUARDIA DE SEGURIDAD* la puede descargar: <a href="https://www.zosepcar.cl/content/OS10/TRAM_guardia_de_seguridad.pdf" target="_blank" class="text-green-500 dark:text-green-400 hover:underline">https://www.zosepcar.cl/content/OS10/TRAM_guardia_de_seguridad.pdf</a>'
     },
-    'guia_jefe_seguridad': {
+    'rule_8': {
         keywords: ['jefe de seguridad'],
         response: '🤖  OK..en este link encontrará la guía de *JEFE DE SEGURIDAD* la puede descargar: <a href="https://www.zosepcar.cl/content/OS10/TRAM_jefe_de_seguridad.pdf" target="_blank" class="text-green-500 dark:text-green-400 hover:underline">https://www.zosepcar.cl/content/OS10/TRAM_jefe_de_seguridad.pdf</a><br><br> 🤖  *ACTAS JEFE SEGURIDAD EX FF.AA*<br><a href="https://drive.google.com/drive/folders/1aAkt55yTidnT90LI-Ss3gNWCGM8X7luW?usp=sharing" target="_blank" class="text-green-500 dark:text-green-400 hover:underline">Ver Actas</a><br><br> 🤖  *ACTAS JEFE DE SEGURIDAD CIVIL*<br><a href="https://drive.google.com/drive/folders/14UBGFXwo7NBA55_o0p2MNQxe1_3H16k4?usp=sharing" target="_blank" class="text-green-500 dark:text-green-400 hover:underline">Ver Actas</a><br><br> 🧙🏼 ‍♂️ *CREDENCIAL JEFE DE SEGURIDAD*<br><a href="https://os10.short.gy/Jef" target="_blank" class="text-green-500 dark:text-green-400 hover:underline">https://os10.short.gy/Jef</a>'
     },
-    'guia_supervisor': {
+    'rule_9': {
         keywords: ['supervisor', 'acreditación supervisor', 'supervisor seguridad', 'para supervisor', 'acreditar un supervisor', 'supervisores', 'acreditar supervisores'],
         response: '🤖 . *SUPERVISOR* <br>1.- *GUIA*<br><a href="https://www.zosepcar.cl/content/OS10/TRAM_supervisor.pdf" target="_blank" class="text-green-500 dark:text-green-400 hover:underline">https://www.zosepcar.cl/content/OS10/TRAM_supervisor.pdf</a><br>2.- *CREDENCIAL*<br><a href="https://os10.short.gy/Sup" target="_blank" class="text-green-500 dark:text-green-400 hover:underline">https://os10.short.gy/Sup</a>'
     },
-    'guia_encargado': {
+    'rule_10': {
         keywords: ['*encargado de seguridad*', '*encargado*'],
         response: '🤖  *ENCARGADO DE SEGURIDAD*<br>*CREDENCIAL:*<br><a href="https://bit.ly/3H6pIOu" target="_blank" class="text-green-500 dark:text-green-400 hover:underline">https://bit.ly/3H6pIOu</a><br>*GUIA:*<br><a href="https://www.zosepcar.cl/content/OS10/TRAM_encargado_de_seguridad.pdf" target="_blank" class="text-green-500 dark:text-green-400 hover:underline">https://www.zosepcar.cl/content/OS10/TRAM_encargado_de_seguridad.pdf</a>'
     },
-    'guia_capacitador': {
+    'rule_11': {
         keywords: ['capacitador'],
         response: '🤖  *CAPACITADOR*<br><a href="https://www.zosepcar.cl/content/OS10/TRAM_capacitador.pdf" target="_blank" class="text-green-500 dark:text-green-400 hover:underline">https://www.zosepcar.cl/content/OS10/TRAM_capacitador.pdf</a><br><br>*CAPACITADOR CIVIL*<br> ➢  Solic. Simple: <a href="https://dal5.short.gy/Solic.Sim" target="_blank" class="text-green-500 dark:text-green-400 hover:underline">https://dal5.short.gy/Solic.Sim</a><br> ➢  Solic simp Punto 6: <a href="https://dal5.short.gy/CapCivil" target="_blank" class="text-green-500 dark:text-green-400 hover:underline">https://dal5.short.gy/CapCivil</a><br><br>*CAPACITADOR EX FF.AA.*<br> ➢  Solic. Simple: <a href="https://dal5.short.gy/Solic.Sim" target="_blank" class="text-green-500 dark:text-green-400 hover:underline">https://dal5.short.gy/Solic.Sim</a><br> ➢  Acta simple punto 8 puerta ancha: <a href="https://dal5.short.gy/Pension" target="_blank" class="text-green-500 dark:text-green-400 hover:underline">https://dal5.short.gy/Pension</a><br> ➢  Acta simple Punto 6 y 7: <a href="https://dal5.short.gy/WVArop" target="_blank" class="text-green-500 dark:text-green-400 hover:underline">https://dal5.short.gy/WVArop</a>'
     },
-    'guia_tecnico': {
+    'rule_12': {
         keywords: ['tecnico'],
         response: '*TÉCNICO* <a href="https://www.zosepcar.cl/content/OS10/TRAM_tecnico.pdf" target="_blank" class="text-green-500 dark:text-green-400 hover:underline">https://www.zosepcar.cl/content/OS10/TRAM_tecnico.pdf</a>'
     },
-     'guia_asesor': {
+    'rule_13': {
         keywords: ['asesor'],
         response: '🤖  *ASESOR*<br>**GUÍA:*<br><a href="https://www.zosepcar.cl/content/OS10/TRAM_asesor.pdf" target="_blank" class="text-green-500 dark:text-green-400 hover:underline">https://www.zosepcar.cl/content/OS10/TRAM_asesor.pdf</a><br>Títulos afines:<br>*Resol. 4070* 20.10.2021- link: <a href="https://bit.ly/3r5HrBg" target="_blank" class="text-green-500 dark:text-green-400 hover:underline">https://bit.ly/3r5HrBg</a><br> ➢  *SIN DIPLOMADO EN SEGURIDAD PRIVADA*<br>1.- INGENIERO DE EJECUCIÓN EN GESTIÓN DE SEGURIDAD PRIVADA.<br>2.- INGENIERO EN GESTIÓN DE SEGURIDAD Y VIGILANCIA PRIVADA.<br> ➢  *CON DIPLOMADO EN SEGURIDAD PRIVADA*<br>3.- INGENIERO EN SEGURIDAD Y PREVENCIÓN DE RIESGO.<br>4.- INGENIERO EN PREVENCIÓN DE RIESGOS.<br>5.- PARA LOS EX MIEMBROS DE LAS FUERZAS ARMADAS, CARABINEROS DE CHILE, POLICÍA DE INVESTIGACIONES Y GENDARMERÍA DE CHILE exigirá EL TITULO DE OFICIAL EGRESADO de/las Fuerzas primadas, de las Fuerzas de Orden y Seguridad Pública o Gendarmería de Chile.<br><br>*Resol. 2660* 20.07.2023-link: <a href="https://bit.ly/44PRW9w" target="_blank" class="text-green-500 dark:text-green-400 hover:underline">https://bit.ly/44PRW9w</a><br>6.- INGENIERÍA EN ADMINISTRACIÓN MENCIÓN SEGURIDAD PRIVADA<br>**CREDENCIAL:*<br><a href="https://bit.ly/3tGuM9a" target="_blank" class="text-green-500 dark:text-green-400 hover:underline">https://bit.ly/3tGuM9a</a>'
     },
-    'guia_instalador_tecnico': {
+    'rule_14': {
         keywords: ['*instalador tecnico*', '*técnico*', '*instalador*'],
         response: '*INSTALADOR TÉCNICO* <a href="https://www.zosepcar.cl/content/OS10/TRAM_instalador_tecnico.pdf" target="_blank" class="text-green-500 dark:text-green-400 hover:underline">https://www.zosepcar.cl/content/OS10/TRAM_instalador_tecnico.pdf</a>'
     },
-    'guia_operador_cajeros': {
+    'rule_15': {
         keywords: ['operador de cajeros'],
         response: '*OPERADOR DE CAJEROS AUTOMÁTICOS* <br><a href="https://www.zosepcar.cl/content/OS10/TRAM_operador_cajeros.pdf" target="_blank" class="text-green-500 dark:text-green-400 hover:underline">https://www.zosepcar.cl/content/OS10/TRAM_operador_cajeros.pdf</a>'
     },
-    'guia_operador_cctv': {
+    'rule_16': {
         keywords: ['*operador cctv*', '*cctv*'],
         response: '🤖  *OPERADOR CCTV*<br>*GUÍA:* <a href="https://www.zosepcar.cl/content/OS10/TRAM_operador_cctv.pdf" target="_blank" class="text-green-500 dark:text-green-400 hover:underline">https://www.zosepcar.cl/content/OS10/TRAM_operador_cctv.pdf</a><br>*CREDENCIAL*<br><a href="https://bit.ly/48jcidU" target="_blank" class="text-green-500 dark:text-green-400 hover:underline">https://bit.ly/48jcidU</a>'
     },
-    'menu_manuales': {
+    'rule_17': {
         keywords: ['manuales'],
         response: '🤖   ⬇️  *ESCRIBE UNA OPCIÓN* 👮🏻 ‍♂️ 🚦 <br>*1M.-* MANUAL DE FUNCIONAMIENTO<br>*2M.-* MANUAL DE CAPACITACIÓN <br>*3M.-* MANUAL DE ORGANIZACIÓN'
     },
-    'guia_empresas': {
+    'rule_18': {
         keywords: ['empresas', 'empresa'],
         response: '*EMPRESAS* <a href="https://www.zosepcar.cl/content/OS10/TRAM_empresas.pdf" target="_blank" class="text-green-500 dark:text-green-400 hover:underline">https://www.zosepcar.cl/content/OS10/TRAM_empresas.pdf</a><br>- Mod. Solicitud Simple: <a href="https://dal5.short.gy/exqKjv" target="_blank" class="text-green-500 dark:text-green-400 hover:underline">https://dal5.short.gy/exqKjv</a><br>- Dec. Jurada 5, 6 y 8 : <a href="https://dal5.short.gy/gZXROQ" target="_blank" class="text-green-500 dark:text-green-400 hover:underline">https://dal5.short.gy/gZXROQ</a><br>Tiene que traer la documentación de los puntos del 1 al 19, considerando lo siguiente:<br><br>*Puntos a considerar como empresa de RR.HH:* <br>* Punto 1, La Solicitud debe ser en duplicado. <br>* Punto 9, El currículum debe ser sin fotografía.<br>* Punto 10, Antecedentes académicos debe ser legalizado ante notario (Diplomado en seguridad privada 400 horas), si no tiene diplomado, el contrato de trabajo legalizado del asesor con su acreditación vigente.<br>* El punto 5, puede ser omitido si no perteneció a las fuerzas armadas.<br>* El punto 12, el F30 deben ingresar el código 801001 para que aparezca *Servicios de Seguridad Privada Prestado por Empresas. <br>* El punto 13, Escritura o constitución de sociedad en el Objetivo debe decir *“Desarrollar actividades como empresa de recursos Humanos, en materias inherentes a Seguridad Privada”.*<br>Si empresa se constituyo por la ley N° 20.659 debe incorporar: <br>a) Certificado de Estatuto actualizado con la formación de la empresa; <br>b) Certificado de vigencia de la empresa y <br>c) Certificado de anotaciones vigentes de la empresa<br>* El punto 16, Set fotográfico, debe ser presentado fotografías del interior y exterior de la oficina indicando la numeración del domicilio (foto panorámica de la numeración de la oficina). Todo esto inserto en un oficio con descripción de cada fotografía.'
     },
-    'manual_funcionamiento': {
+    'rule_19': {
         keywords: ['1m'],
         response: '*MANUAL DE FUNCIONAMIENTO* <a href="https://www.zosepcar.cl/content/OS10/manual_funcionamiento.pdf" target="_blank" class="text-green-500 dark:text-green-400 hover:underline">https://www.zosepcar.cl/content/OS10/manual_funcionamiento.pdf</a>'
     },
-    'manual_organizacion': {
+    'rule_20': {
         keywords: ['3m'],
         response: '*MANUAL DE ORGANIZACIÓN*<br><a href="https://www.zosepcar.cl/content/OS10/manual_organizacion.pdf" target="_blank" class="text-green-500 dark:text-green-400 hover:underline">https://www.zosepcar.cl/content/OS10/manual_organizacion.pdf</a>'
     },
-    'manual_capacitacion': {
+    'rule_21': {
         keywords: ['2m'],
         response: '*MANUAL DE CAPACITACIÓN*<br><a href="https://www.zosepcar.cl/content/OS10/manual_capacitacion.pdf" target="_blank" class="text-green-500 dark:text-green-400 hover:underline">https://www.zosepcar.cl/content/OS10/manual_capacitacion.pdf</a>'
     },
-    'menu_leyes': {
+    'rule_22': {
         keywords: ['leyes'],
         response: '*ESCRIBE UN NUMERO LEY O DECRETO*.<br>  🚦 .  ⬇️  <br> <br>DECTO. *261* DEL 31.0 un7.2020<br>DECTO. *298* DEL 17.09.2019<br>DECTO. *123* DEL 05.04.2019<br>DECTO. *1045* DEL 12.09.2018<br>DECTO. *867* DEL 12.09.2017 <br>DECTO. *1814* DEL 10.11.2014<br>DECTO. *222* DEL 30.10.2014<br>DECTO. *1122* DEL 19.10.1994<br>DECTO. *41* DEL 05.03.1996<br>DECTO. *1772* DEL 26.01.1995<br>DECTO. *1773* DEL 14.11.1994<br>DECTO. *93* DEL 21.10.1985<br>D. LEY. *3607* DEL 08.01.1981<br>LEY *19303* DEL 13.04.1994<br>Resol. *253* DEL 29.10.2013<br>Resol. *59*. DEL 30.09.2014<br>Resol. *32*. DEL 31.01.2024<br>Resol. *80* DEL 20.03.2024<br>LEY. *21659* DEL 21.03.2024'
     },
-    'decreto_261': {
+    'rule_23': {
         keywords: ['261'],
         response: '*DECRETO NRO 261*. <br><br><a href="https://www.zosepcar.cl/content/OS10/Decreto-261.pdf" target="_blank" class="text-green-500 dark:text-green-400 hover:underline">https://www.zosepcar.cl/content/OS10/Decreto-261.pdf</a>'
     },
-    'decreto_298': {
+    'rule_24': {
         keywords: ['298'],
         response: '*DECRETO 298*. <a href="https://www.bcn.cl/leychile/navegar?idNorma=1136545&idParte=10054790&idVersion=2019-09-17" target="_blank" class="text-green-500 dark:text-green-400 hover:underline">https://www.bcn.cl/leychile/navegar?idNorma=1136545&idParte=10054790&idVersion=2019-09-17</a>'
     },
-    'decreto_123': {
+    'rule_25': {
         keywords: ['123'],
         response: '*DECRETO 123*. <a href="https://www.bcn.cl/leychile/navegar?idNorma=1130300&idParte=10013741&idVersion=2019-04-05" target="_blank" class="text-green-500 dark:text-green-400 hover:underline">https://www.bcn.cl/leychile/navegar?idNorma=1130300&idParte=10013741&idVersion=2019-04-05</a>'
     },
-    'decreto_1045': {
+    'rule_26': {
         keywords: ['1045'],
         response: '*DECRETO 1045*. <a href="https://www.bcn.cl/leychile/navegar?idNorma=1122982&idParte=9948603&idVersion=2018-09-12" target="_blank" class="text-green-500 dark:text-green-400 hover:underline">https://www.bcn.cl/leychile/navegar?idNorma=1122982&idParte=9948603&idVersion=2018-09-12</a>'
     },
-    'decreto_867': {
+    'rule_27': {
         keywords: ['867'],
         response: '*DECRETO 867*. <br><br><a href="https://www.bcn.cl/leychile/navegar?idNorma=1116274" target="_blank" class="text-green-500 dark:text-green-400 hover:underline">https://www.bcn.cl/leychile/navegar?idNorma=1116274</a>'
     },
-    'decreto_1814': {
+    'rule_28': {
         keywords: ['1814', 'decreto 1814', 'decreto transporte valores'],
         response: '*DECRETO 1814*<br><a href="https://www.bcn.cl/leychile/navegar?idNorma=1069299" target="_blank" class="text-green-500 dark:text-green-400 hover:underline">https://www.bcn.cl/leychile/navegar?idNorma=1069299</a>'
     },
-    'decreto_222': {
+    'rule_29': {
         keywords: ['222'],
         response: '*DECRETO 222*. <a href="https://www.bcn.cl/leychile/navegar?idNorma=1055580" target="_blank" class="text-green-500 dark:text-green-400 hover:underline">https://www.bcn.cl/leychile/navegar?idNorma=1055580</a>'
     },
-    'decreto_1122': {
+    'rule_30': {
         keywords: ['1122'],
         response: '*DECRETO 1122*<br><a href="https://www.bcn.cl/leychile/navegar?idNorma=1072929" target="_blank" class="text-green-500 dark:text-green-400 hover:underline">https://www.bcn.cl/leychile/navegar?idNorma=1072929</a>'
     },
-     'decreto_41': {
+     'rule_31': {
         keywords: ['41'],
         response: '*DECRETO 41*. <a href="https://www.bcn.cl/leychile/navegar?idNorma=19870" target="_blank" class="text-green-500 dark:text-green-400 hover:underline">https://www.bcn.cl/leychile/navegar?idNorma=19870</a>'
     },
-    'decreto_1773': {
+    'rule_32': {
         keywords: ['1773'],
         response: '*DECRETO 1773*<br><a href="https://www.bcn.cl/leychile/navegar?idNorma=18594" target="_blank" class="text-green-500 dark:text-green-400 hover:underline">https://www.bcn.cl/leychile/navegar?idNorma=18594</a>'
     },
-    'decreto_93': {
+    'rule_33': {
         keywords: ['93', 'decreto 93'],
         response: '*DECRETO 93.* <br><a href="https://www.bcn.cl/leychile/navegar?idNorma=9081" target="_blank" class="text-green-500 dark:text-green-400 hover:underline">https://www.bcn.cl/leychile/navegar?idNorma=9081</a>'
     },
