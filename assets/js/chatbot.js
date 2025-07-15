da61b7e77852a1e1b5eeb82791db0c5abdb4d642
danielfigueroa1879 <90109125+danielfigueroa1879@users.noreply.github.com>
2025-07-14 22:27:16 -0400

    Update chatbot.js

--- assets/js/chatbot.js
+++ assets/js/chatbot.js
@@ -177,7 +177,7 @@ const predefinedResponses = {
     'rule_255': { keywords: ["*examen moto*","examen para moto","moto"], response: '🤖🧙🏼‍♂️ *Examen moto*\nhttps://dal5.short.gy/ExMoto' },
     'rule_256': { keywords: ["honorarios"], response: '*HONORARIOS*\nhttps://rentab.netlify.app/' },
     'rule_257': { keywords: ["gestudio","estudiar","gestor académico","gestor"], response: '🤖🧙🏼‍♂️✅\n\nhttps://gestudios.netlify.app/' },
-    'rule_258': { keywords: ["*quien es tu creador*","*quien te creo*","*creador*","*creo*"], response: '🤖🧙🏼‍♂️✅ Mi creador es todo el equipo de Profesionales que se encuentra trabajando en la oficina de seguridad Privada OS10 Coquimbo y el\n*Ingeniero en Informática y Ciberseguridad Daniel Figueroa Chacama*' }
+    'rule_258': { keywords: ["*quien es tu creador*","*quien te creo*","*creador*","*creo*"], response: '🤖🧙🏼‍♂️✅ Mi creador es todo el equipo de Profesionales que se encuentra trabajando en la oficina de seguridad Privada OS10 Coquimbo y el\n*Ingeniero en Informática y Ciberseguridad\n   Daniel Figueroa Chacama*' }
 };
 
 // --- API Configuration ---
