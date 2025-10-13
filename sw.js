a3acf06722e89f5cef9747c1e1b3f7a8e366b46c
danielfigueroa1879 <90109125+danielfigueroa1879@users.noreply.github.com>
2025-10-13 08:57:06 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js - Service Worker
-const CACHE_NAME = 'directivas-os10-cache-v1.6'; // Versión incrementada para forzar la actualización
+const CACHE_NAME = 'directivas-os10-cache-v1.7'; // ¡Versión incrementada para forzar actualización!
 
 // Lista de archivos y recursos a cachear durante la instalación
 const urlsToCache = [
